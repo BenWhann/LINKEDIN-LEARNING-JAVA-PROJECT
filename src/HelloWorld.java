3fc100f22f41e5c8bc818f8ad775eb59d8ca811d
BenWhann <whann004@umn.edu>
2024-02-06 16:24:59 -0600

    while loops

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,14 +2,16 @@ import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) {
-    System.out.println("Pick a number between 1 and 10");
-        Scanner input = new Scanner(System.in);
-        int inputtedNum = input.nextInt();
-        if (inputtedNum <= 5) {
-            System.out.println("You will have a good fortune");
-        } else {
-            System.out.println("You will have a bad fortune");
+        Scanner scanner = new Scanner(System.in);
+        boolean isOnRepeat = true;
+        while(isOnRepeat) {
+            System.out.println("Playing current song on repeat");
+            System.out.println("Would you like to take this song off repeat?");
+            String input = scanner.next();
+            if (input.equals("yes")) {
+                isOnRepeat = false;
+            }
         }
-
+        System.out.println("Playing next song");
     }
 }

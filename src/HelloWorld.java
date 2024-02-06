e4879ebe585421c46842d98ff44e554ac2cc6f58
BenWhann <whann004@umn.edu>
2024-02-06 15:10:37 -0600

    primative types

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,14 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int age = 15;
+        double gpa = 3.45;
+        char firstInitial = 'K';
+        char lastInitial = 'H';
+        boolean perfectAttendance = false;
+        System.out.println(age);
+        System.out.println(gpa);
+        System.out.println(firstInitial);
+        System.out.println(lastInitial);
+        System.out.println(perfectAttendance);
     }
 }

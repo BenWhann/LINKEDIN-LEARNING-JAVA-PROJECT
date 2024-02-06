6eab463cbbddd46bd3eaf8cc4d34c59899174126
BenWhann <whann004@umn.edu>
2024-02-06 16:13:55 -0600

    conditions

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        String studentFirstName = "Jimmy";
-        String studentLastName = "Fredette";
-        double gpa = 3.45;
-        System.out.println(gpa);
-        System.out.println(studentFirstName + " " + studentLastName + " has a GPA of " + gpa);
-        System.out.println("What do you want to update it to?");
+    System.out.println("Pick a number between 1 and 10");
         Scanner input = new Scanner(System.in);
-        gpa = input.nextDouble();
-        System.out.println(studentFirstName + " " + studentLastName + " now has a GPA of " + gpa);
+        int inputtedNum = input.nextInt();
+        if (inputtedNum <= 5) {
+            System.out.println("You will have a good fortune");
+        } else {
+            System.out.println("You will have a bad fortune");
+        }
+
     }
 }
